b6747183f47baab1fc4c22ebecfc1f263d72b8b4
lovinitvernon <ilovegrayfullbuster182@gmail.com>
2026-02-09 04:49:22 -0500

    feat: Add a collection of Material Design icons, a custom reply SVG, and the SamsungSharpSans font.

---
+++ Theme/bd-scss.config.js
@@ -0,0 +1,12 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: 'Unity',
+		author: 'super.user',
+        authorId: '419610859392860162',
+		version: '2.0.0',
+		description: 'A highly customizable theme inspired by Samsung\'s OneUI, modernized for 2026.',
+		source: 'https://github.com/lovinitvernon/hello-theme',
+        invite: 'BDGSZDYSQ',
+	},
+};
